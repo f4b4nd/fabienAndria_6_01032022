f9d698aaecb156b3eb7ae09d1ad3927a2eea67ae
fabien <fabien.andrian@gmail.com>
2022-03-07 18:19:19 +0100

    change script path to relative path in html

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,9 +1,9 @@
 async function getData () {
-    
+
     const localeURL = 'data/photographers.json'
     const remoteURL = 'https://f4b4nd.github.io/fabienAndria_6_01032022/data/photographers.json'
     
-    let response = await fetch(localeURL)
+    let response = await fetch(remoteURL)
 
     if (!response.ok) {
         response = await fetch(remoteURL)
