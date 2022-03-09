4633b261558f1462447e37c3bf5867b09d44ba45
fabien <fabien.andrian@gmail.com>
2022-03-09 19:24:10 +0100

    refacto for lightbox ok

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -12,16 +12,16 @@ closeLightBoxBtn.addEventListener('click', closeLightbox)
 export function displayLightbox (mediaElement) {
     const lightbox = new LightboxFactory(mediaElement)
     lightbox.displayLightbox()
-    lightbox.setLightboxMedia()
+    lightbox.setMedia(mediaElement.srcElement)
     lightbox.setTitle()
 
     /**PREVIOUS */
     const lightboxPrevious = document.querySelector('.lightbox__previous')
-    lightboxPrevious.addEventListener('click', () => lightbox.setLightboxPreviousMedia())
+    lightboxPrevious.addEventListener('click', () => lightbox.setPreviousLightbox())
 
     /**NEXT */
     const lightboxNext = document.querySelector('.lightbox__next')
-    lightboxNext.addEventListener('click', () => lightbox.setLightboxNextMedia())
+    lightboxNext.addEventListener('click', () => lightbox.setNextLightbox())
 }
 
 export class LightboxFactory {
@@ -38,26 +38,10 @@ export class LightboxFactory {
     }
 
     /**SETTERS */
-    setLightboxMedia() {
+    setMedia(mediaElement) {
         clearHTMLNode(this.lightboxMedia)
-        const newMediaElement = this.mediaElement.cloneNode(true)
+        const newMediaElement = mediaElement.cloneNode(true)
         this.lightboxMedia.appendChild(newMediaElement)
-    }
-
-    setLightboxNextMedia() {
-        clearHTMLNode(this.lightboxMedia)
-        const newMediaElement = this.getNextMedia().cloneNode(true)
-        this.lightboxMedia.appendChild(newMediaElement)
-        this.currentNodeIndex = this.getNextNodeIndex()
-        this.setTitle()
-    }
-
-    setLightboxPreviousMedia() {
-        clearHTMLNode(this.lightboxMedia)
-        const newMediaElement = this.getPreviousMedia().cloneNode(true)
-        this.lightboxMedia.appendChild(newMediaElement)
-        this.currentNodeIndex = this.getPreviousNodeIndex()
-        this.setTitle()
     }
 
     setTitle () {
@@ -65,7 +49,21 @@ export class LightboxFactory {
         lightboxTitle.textContent = this.getTitle()
     }
 
-    /*** */
+    setPreviousLightbox() {
+        this.currentNodeIndex = this.getPreviousNodeIndex()
+        const media = this.getMedia()
+        this.setMedia(media)
+        this.setTitle()
+    }
+
+    setNextLightbox() {
+        this.currentNodeIndex = this.getNextNodeIndex()
+        const media = this.getMedia()
+        this.setMedia(media)
+        this.setTitle()
+    }
+
+    /***GETTERS*/
     getCardParentNode () {
         const parentDOM = this.mediaElement.closest('.card')
         return parentDOM
@@ -82,39 +80,29 @@ export class LightboxFactory {
         return title
     }
 
-    /***CURRENT */
+    getMedia () {
+        const currentNode = this.getCardsNodes().item(this.currentNodeIndex)
+        const media = currentNode.querySelector('img, video')
+        return media
+    }
+
+    /***INDEXES */
     getCurrentNodeIndex() {
         const nodes = this.getCardsNodes()
         const currentNodeIndex = Array.from(nodes).findIndex(node => node === this.getCardParentNode())
         return currentNodeIndex
     }
 
-    /***PREVIOUS */
     getPreviousNodeIndex () {
         const currentIndex = this.currentNodeIndex
         const previousNodeIndex = currentIndex > 0 ? currentIndex - 1 : this.getCardsNodes().length - 1
         return previousNodeIndex
     }
-    
-    getPreviousMedia(){
-        const previousIndex = this.getPreviousNodeIndex()
-        const previousNode = this.getCardsNodes().item(previousIndex)
-        const previousMedia = previousNode.querySelector('img, video')
-        return previousMedia
-    }
 
-    /*** NEXT */
     getNextNodeIndex () {
         const currentIndex = this.currentNodeIndex
         const nextNodeIndex = currentIndex < this.getCardsNodes().length - 1 ? currentIndex + 1 : 0
         return nextNodeIndex
-    }
-
-    getNextMedia(){
-        const nextIndex = this.getNextNodeIndex()
-        const nextNode = this.getCardsNodes().item(nextIndex)
-        const nextMedia = nextNode.querySelector('img, video')
-        return nextMedia
     }
     
 }
