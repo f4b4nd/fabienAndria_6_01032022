81a7cdc6482b56bf4613143df6726044b7b4232e
fabien <fabien.andrian@gmail.com>
2022-03-14 14:03:21 +0100

    remove unecessary dropdown function

--- scripts/factory/dropdown.js
+++ scripts/factory/dropdown.js
@@ -12,7 +12,6 @@ export default class DropdownFactory {
         this.setOptionsEventListeners()
     }
 
-    getOptionID () {}
     setCurrentOption (newIndex) {
 
         if (this.currentOptionIndex === newIndex) return
